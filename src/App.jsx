0a4e772db1a1cdfeb8d92096ad448115651457e2
Derek Day <derek.andrew.day@gmail.com>
2023-08-17 15:26:30 -0400

    quiz set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Question from "../Components/Question";
+import qBank from "../Components/QuestionBank";
+import Score from "../Components/Score";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			questionBank: qBank,
+			currentQuestion: 0,
+			selectedOption: "",
+			score: 0,
+			quizEnd: false,
+		};
+	}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	handleOptionChange = (e) => {
+		this.setState({ selectedOption: e.target.value });
+	};
+
+	handleFormSubmit = (e) => {
+		e.preventDefault();
+		this.checkAnswer();
+		this.handleNextQuestion();
+	};
+
+	checkAnswer = () => {
+		const { questionBank, currentQuestion, selectedOption, score } = this.state;
+		if (selectedOption === questionBank[currentQuestion].answer) {
+			this.setState((prevState) => ({ score: prevState.score + 1 }));
+		}
+	};
+
+	handleNextQuestion = () => {
+		const { questionBank, currentQuestion } = this.state;
+		if (currentQuestion + 1 < questionBank.length) {
+			this.setState((prevState) => ({
+				currentQuestion: prevState.currentQuestion + 1,
+				selectedOption: "",
+			}));
+		} else {
+			this.setState({
+				quizEnd: true,
+			});
+		}
+	};
+
+	render() {
+		const { questionBank, currentQuestion, selectedOption, score, quizEnd } =
+			this.state;
+		return (
+			<div className="App d-flex flex-column align-items-center justify-content-center">
+				<h1 className="app-title">Eder's Practice Quiz</h1>
+				{!quizEnd ? (
+					<Question
+						question={questionBank[currentQuestion]}
+						selectedOption={selectedOption}
+						onOptionChange={this.handleOptionChange}
+						onSubmit={this.handleFormSubmit}
+					/>
+				) : (
+					<Score
+						score={score}
+						onNextQuestion={this.handleNextQuestion}
+						className="score"
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
